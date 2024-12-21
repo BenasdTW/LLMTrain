aa1e0826ba4c63b47d560ffe7cf3855168d1bfa0
fuc2323 <xx87923xx@yahoo.com.tw>
2024-12-21 10:52:22 +0000

    refactor dataset and logging callback

--- lora_text2sql.py
+++ lora_text2sql.py
@@ -1,67 +1,22 @@
 import torch
-import json
 from datetime import datetime
 from trl import SFTTrainer
 from peft import LoraConfig
-from datasets import load_dataset
-from torch.utils.data import Dataset
 from transformers.trainer_utils import IntervalStrategy
 from transformers import AutoTokenizer, TrainingArguments, BitsAndBytesConfig
 from liger_kernel.transformers import AutoLigerKernelForCausalLM
+from RefinedText2SQL import NSText2SQLDataset, NSText2SQLDatasetFormatted, CustomLoggingCallback
 
-model_name = "meta-llama/Llama-3.2-3B-Instruct"
+model_name = "meta-llama/Llama-3.2-1B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 tokenizer.pad_token = "<|finetune_right_pad_id|>"
 print(f"{tokenizer.eos_token_id=}")
 print(f"{tokenizer.pad_token_id=}")
 
-def tokenize(input_str, output, max_length):
-    instruction_len = len(torch.tensor(tokenizer.encode(input_str), dtype=torch.int32))
-
-    input_str = input_str + output + tokenizer.eos_token
-    model_inputs = tokenizer(input_str, max_length=max_length, padding="max_length", return_tensors="pt")
-    return model_inputs, instruction_len
-    
-def filter_by_token_count(x, max_length):
-    # Tokenize the text and check the length
-    model_inputs, _ = tokenize(x["instruction"], x["output"], max_length)
-    if model_inputs is None:
-        return False
-    return len(model_inputs["input_ids"].squeeze(0)) <= max_length
-
-class NSText2SQLDataset(Dataset):
-    def __init__(self, size=None, max_length=2048, split="train"):
-        dataset = load_dataset("NumbersStation/NSText2SQL", split=split)
-        self.dataset = dataset.filter(lambda x: filter_by_token_count(x, max_length))
-        print(len(self.dataset))
-        if size:
-            self.dataset = self.dataset.select(range(size))
-        self.max_length = max_length
-
-        self.eos_token = tokenizer.eos_token
-        self.eos_token_id = tokenizer.eos_token_id
-        self.pad_token_id = tokenizer.pad_token_id
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-        model_inputs, instruction_len = tokenize(self.dataset[index]["instruction"], self.dataset[index]["output"], self.max_length)
-
-        model_inputs["input_ids"] = model_inputs["input_ids"].squeeze(0)
-        model_inputs["attention_mask"] = model_inputs["attention_mask"].squeeze(0)
-        labels = model_inputs["input_ids"].clone()  # Labels for causal LM
-        # Mask out the instruction string
-        labels[:instruction_len] = -100
-        # Mask out the paddings
-        labels[labels == tokenizer.pad_token_id] = -100
-        model_inputs["labels"] = labels
-            
-        return model_inputs
-
 # max_length = 512: 220742
-dataset = NSText2SQLDataset(size=102500, max_length=512)
+# dataset = NSText2SQLDataset(tokenizer=tokenizer, size=102500, max_length=512)
+dataset = NSText2SQLDatasetFormatted(tokenizer=tokenizer, size=102500, max_length=512)
 dataset, eval_set = torch.utils.data.random_split(dataset, [102400, 100])
 print(f"{len(dataset)=} {len(eval_set)=}")
 
@@ -128,26 +83,20 @@ trainer = SFTTrainer(
     tokenizer=tokenizer,
 )
 
-
-# Start training
-trainer.model.print_trainable_parameters()
-trainer.train()
-
-output_dir = "text2sql-3b-Instruct-2"
-trainer.model.save_pretrained(output_dir)
-tokenizer.save_pretrained(output_dir)
-
-
 # Format it as "MMDD-hhmmss"
 now = datetime.now()
 formatted_time = now.strftime("%m%d-%H%M%S")
 
-print(f"Saved log to ./logs/{formatted_time}-trainer.log")
+output_name = "test"
+output_log = f"./logs/{output_name}-{formatted_time}.log"
 
-with open(f"./logs/{formatted_time}-trainer.log", "w") as f:
-    for obj in trainer.state.log_history:
-        obj_str = json.dumps(obj)
-        f.write(obj_str)
-        f.write("\n")
+
+# Start training
+trainer.model.print_trainable_parameters()
+trainer.add_callback(CustomLoggingCallback(output_log))
+trainer.train()
+
+trainer.model.save_pretrained(output_name)
+tokenizer.save_pretrained(output_name)
 
 
